a5376d1f019785f7cacf891c45e76ee15e6e4108
Adam <adam8690@hotmail.co.uk>
2017-03-31 17:32:06 +0100

    added checkout class an dcreated constructor. This will hold all of the methods to calculate the price of items in the shopping list.

--- src/Checkout.java
+++ src/Checkout.java
@@ -2,4 +2,15 @@
  * Created by user on 31/03/2017.
  */
 public class Checkout {
+
+    Customer customer;
+    ShoppingBasket shoppingBasket;
+
+    public Checkout(Customer customer, ShoppingBasket shoppingBasket) {
+        this.customer = customer;
+        this.shoppingBasket = shoppingBasket;
+    }
+
+    
+
 }
