b066d1c5169905533798116d72d63b7bc50d77dd
Adam <adam8690@hotmail.co.uk>
2017-03-31 11:44:06 +0100

    made first class.

---
+++ Tests/ShoppingBasketTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Created by user on 31/03/2017.
+ */
+public class ShoppingBasketTest {
+
+    @Test
+    public void testJunitIsWorkingTest(){
+        assertTrue(true);
+    }
+
+}
---
+++ src/ShoppingBasket.java
@@ -0,0 +1,7 @@
+/**
+ * Created by user on 31/03/2017.
+ */
+public class ShoppingBasket {
+
+
+}
